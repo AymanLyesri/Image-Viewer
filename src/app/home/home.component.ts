b04bf6810e14b1038517c14db07183341bdba5e7
ayman lyesri <lyesri99@gmail.com>
2022-12-02 22:22:45 +0100

    added env variable to the title

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  getTitle() {
+    return environment.NAME;
   }
-
 }
