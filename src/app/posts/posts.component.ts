a7ab7042e1d88872c27716a9b7ac98bc6a50d290
ayman lyesri <lyesri99@gmail.com>
2022-12-02 20:41:57 +0100

    added animations, css styling, delete when logged

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -12,6 +12,7 @@ import {
 import { ImageService } from '../services/image.service';
 import { Image } from '../models/Image';
 import { Subscription, timeout } from 'rxjs';
+import { AuthentificationService } from '../services/authentification.service';
 
 @Component({
   selector: 'app-posts',
@@ -20,27 +21,43 @@ import { Subscription, timeout } from 'rxjs';
 })
 export class PostsComponent implements OnInit, OnDestroy, AfterViewInit {
   @ViewChildren('img') imgs: QueryList<ElementRef>;
+  @ViewChildren('hidden') hiddens: QueryList<ElementRef>;
+  observer: IntersectionObserver;
 
   public images: Image[] = [];
   private imageSubscribtion: Subscription;
 
-  constructor(private imageservice: ImageService, private render: Renderer2) {}
+  constructor(
+    private imageservice: ImageService,
+    private render: Renderer2,
+    private AuthService: AuthentificationService
+  ) {}
 
   ngOnInit(): void {
-    console.log('init');
     this.imageservice.getPosts();
     this.imageSubscribtion = this.imageservice
       .getImagesStream()
       .subscribe((images: Image[]) => {
         this.images = images;
-        console.log('client side 1  : ', this.images);
       });
   }
 
+  isLoggedIn() {
+    return this.AuthService.isLoggedIn();
+  }
+
   ngAfterViewInit(): void {
+    this.hideAndShow();
     this.processChildren();
+
     this.imgs.changes.subscribe(() => {
       this.processChildren();
+    });
+
+    this.hiddens.changes.subscribe(() => {
+      this.hiddens.forEach((hidden) => {
+        this.observer.observe(hidden.nativeElement);
+      });
     });
   }
 
@@ -59,6 +76,23 @@ export class PostsComponent implements OnInit, OnDestroy, AfterViewInit {
     });
   }
 
+  private hideAndShow(): void {
+    let options = {
+      root: null as Element,
+      rootMargin: '0px',
+      threshold: 0.1,
+    };
+
+    this.observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        } else {
+          entry.target.classList.remove('show');
+        }
+      });
+    }, options);
+  }
   private idSubscribtion: Subscription;
 
   deleteImage(id: string) {
