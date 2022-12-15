c5a634ca8e54468096163f9f52ead5182c9778f5
ayman lyesri <lyesri99@gmail.com>
2022-12-15 11:07:55 +0100

    added zoom options

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -29,6 +29,8 @@ export class PostsComponent implements OnInit, OnDestroy, AfterViewInit {
   private imagesSubscription: Subscription;
   private imgChanges: Subscription;
 
+  private zoomOnHover: string = 'zoom';
+
   constructor(
     private imageService: ImageService,
     private AuthService: AuthenticationService,
@@ -50,22 +52,31 @@ export class PostsComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   toggleGrid(columns: number) {
-    let columnNumber: string =
-      columns == 0
-        ? 'repeat(auto-fill, minmax(200px, 1fr))'
-        : columns == 2
-        ? 'repeat(auto-fill, minmax(400px, 1fr))'
-        : 'repeat(auto-fill, minmax(300px, 1fr))';
     this.render.setStyle(
       this.grid.nativeElement,
       'grid-template-columns',
-      columnNumber
+      'repeat(auto-fill, minmax(' + columns + '00px, 1fr))'
     );
+  }
+
+  toggleHover(zoom: string) {
+    console.log(zoom);
+
+    this.zoomOnHover = zoom;
+    this.imgs.forEach((img) => {
+      this.render.removeClass(img.nativeElement.parentNode, 'zoom');
+      this.render.removeClass(img.nativeElement.parentNode, 'zoomPlus');
+      this.render.removeClass(img.nativeElement.parentNode, 'noZoom');
+      this.render.addClass(img.nativeElement.parentNode, this.zoomOnHover);
+    });
   }
 
   ngAfterViewInit(): void {
     this.imgChanges = this.imgs.changes.subscribe(() => {
       this.observer.observe(this.imgs.last.nativeElement);
+      this.imgs.forEach((img) => {
+        this.render.addClass(img.nativeElement.parentNode, this.zoomOnHover);
+      });
     });
   }
 
